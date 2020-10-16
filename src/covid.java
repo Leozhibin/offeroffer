82a9bc340d4644bfc78181fd7627bb8641c55dac
Leon <leozhibin@gmail.com>
2020-10-16 21:28:06 +0800

    cvte

---
+++ src/covid.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class covid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        for(int i=0;i<num;i++){
+            String pat = sc.next();
+            String raw = sc.next();
+            for(int j=0;j<raw.length();j++){
+                if(pat.charAt(0)==raw.charAt(j)){
+
+                }
+            }
+        }
+//        String str = sc.nextLine();
+    }
+}
